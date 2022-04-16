f1bfa31e17488eeee0306249d3ee679461c491e2
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-04-16 17:34:32 -0500

    Correccion de pokemon.js se agrego module.exports = Pokemon

--- weekly_mission_1/example9/weekly_mission_1/main.js
+++ weekly_mission_1/example9/weekly_mission_1/main.js
@@ -1,18 +1,18 @@
-let Pokemon = require('./pokemon')
+const Pokemon = require('./pokemon') //Con require se pasa la clase Pokemon del archivo pokemon.js
 
-let pikachu = new Pokemon("pikachu")
+let pikachu = new Pokemon("pikachu") // Se construye un objeto llamado pikachu
 let bulbasaur = new Pokemon("bulbasaur")
 let squirtle = new Pokemon("squirtle")
-let  charmander = new Pokemon("charmander")
+let charmander = new Pokemon("charmander")
 
-pikachu.sayHello("Te saluda!!!")
-pikachu.sayMessage("Heey!")
+pikachu.sayHello() //se manda a llamar al metodo sayHello()
+pikachu.sayMessage("Heey!") //se manda a llamar al metodo sayMassage(aqui se le inclulle el mensaje )
 
-bulbasaur.sayHello("Te saluda!!!")
+bulbasaur.sayHello()
 bulbasaur.sayMessage("Heey!")
 
-charmander.sayHello("Te saluda!!!")
+charmander.sayHello()
 charmander.sayMessage("Heey!")
 
-squirtle.sayHello("Te saluda!!!")
+squirtle.sayHello()
 squirtle.sayMessage("Heey!")
--- weekly_mission_1/example9/weekly_mission_1/pokemon.js
+++ weekly_mission_1/example9/weekly_mission_1/pokemon.js
@@ -1,14 +1,15 @@
-class Pokemon {
-    constructor (name) {
-        this.name = name
+class Pokemon { // Declaramos la Clase Pokemon
+    constructor (name) { //Se crea un contructor nonde se le pasara name 
+        this.name = name //name pasa this.mame para crear el nombre del objeto
     }
-    sayHello (message) {
-        console.log(`[${this.name}] ${message}`)
-    }
-    sayMessage(message) {
-        console.log(`[${this.name}] ${message}`)
-    }
-    
-}
 
-module.exports.Pokemon = Pokemon;
+    sayHello () { //se crea el metodo  sin resivir nada por eso esta vacio en los parentesis
+        console.log(`Mi pokemon [${this.name}] te saluda!!! `) //imprime  con [${this.name}] el nombre del objeto o objetos que se le pasaron
+    }
+
+    sayMessage(message) { //se crea el metodo en este casa no esta vacio y recibira una variable message
+        console.log(`Mi pokemon [${this.name}] dice: ${message}`) //imprime  con [${this.name}] el nombre del objeto o objetos que se le pasaron
+    }                                                              //y con ${message} imprime la variable que se le paso
+
+}
+module.exports = Pokemon // se envia el modulo de la clase Pokemon y se manda a llamar con  "const Pokemon = require('./pokemon')"
