51db813556c4c4fb2652cbe7688d00b8b6773538
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-04-18 17:42:18 -0500

    se termino de modificar archivo viajero.js y package.json

--- weekly_mission_2/example4_modularización_de_clases/main.js
+++ weekly_mission_2/example4_modularización_de_clases/main.js
@@ -3,5 +3,5 @@ import Viajero from './viajero.js'
 */
 const viajero1 = new Viajero("Carlo", "LaunchX", "Node JS", "Abril 2022")
 console.log(viajero1)
-console-log(viajero1.getNameAndUsername()) //Método de la clase padre
+console.log(viajero1.getNameAndUsername()) //Método de la clase padre
 console.log(viajero1.getGeneralInfo()) // Método de la clase hija
