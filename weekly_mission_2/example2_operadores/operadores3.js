c37db9d9fff3e98ab741da87c93a09c519697c4f
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-04-17 19:02:06 -0500

    se creo los ejemplos del 1 al 16 de operadores

---
+++ weekly_mission_2/example2_operadores/operadores3.js
@@ -0,0 +1,4 @@
+//Ejemplo 3: forEach para imprimir los paises en letras mayúsculas
+const countries = ['Filamdia', 'Denmark', 'Sweden', 'Norway', 'Iceland']
+console.log("Ejemplo 5: implimiendo la lista de países en mayúscula")
+countries.forEach((element) => console.log(element.toUpperCase()))
---
+++ weekly_mission_2/example2_operadores/operadores5.js
@@ -0,0 +1,5 @@
+//Ejemplo 5: Uso de map para convertir todos los nombres de una lista a minúsculas
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const namesToUpperCase = names.map((name) => name.toLowerCase()) //El metodo toLowerCase devuelve le valor en minúsculas de la cadena que realiza la llamada
+console.log("Ejemplo 5: Uso de map para convertir todos los nombres de una lsita a minúsculas")
+console.log(namesToUpperCase)
