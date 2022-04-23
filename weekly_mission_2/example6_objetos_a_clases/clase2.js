5b297b4ddee4d2fb9348a0d47c56a379867d89a1
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-04-22 21:09:01 -0500

    se creo ejemplo 2 clase con propiedades

---
+++ weekly_mission_2/example6_objetos_a_clases/clase2.js
@@ -0,0 +1,10 @@
+class myObjetc2{
+    constructor(name, age){
+        this.name = name
+        this.age = age
+    }
+    
+}
+console.log("Ejemplo 2: Clase con propiedades")
+const user1 =  new myObjetc2("Carlo", 27)
+console.log(user1) 
