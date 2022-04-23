427ea6d972a160fa869eb7967b4643812f0ec04c
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-04-22 20:15:15 -0500

    se añadio el ejercicio 5, 6 y 7

--- exercises/explorers.js
+++ exercises/explorers.js
@@ -61,13 +61,27 @@ const explorers = [
 
 console.log("Ejemplo 1: Imprime el nombre de cada explorer con forEach")
 explorers.forEach(obj =>  console.log(obj.name)) //imprime el nombre de todos los exploradores.
-console.log("Ejemplo 2:imprime stack de cada esplorer con forEach")
+
+console.log("Ejemplo 2: Imprime stack de cada esplorer con forEach")
 explorers.forEach(obj => console.log(obj.name, obj.stack)) //Implime el explorer y el estack
-console.log("Crea una nueva lista con las listas de stacks de cada explorer, usa MAP")
+
+console.log("Ejemplo 3: Crea una nueva lista con las listas de stacks de cada explorer, usa MAP")
 const numstacks = explorers.map(num => num.stack)
 console.log(numstacks)
-console.log("Obtén la lista de explorers que tengan en su stack 'js', usa FILTER")
+
+console.log("Ejemplo 4: Obtén la lista de explorers que tengan en su stack 'js', usa FILTER")
 const filtrojs = explorers.filter((obj) => 
- obj.stack.includes('js')
-)
+ obj.stack.includes('js'))
 console.log(filtrojs)
+
+console.log("Ejemplo 5: Busca el primer explorer que sea de la CDMX, usa FIND")
+const citys = explorers.find((citys) => citys.city='CDMX')
+console.log(citys)
+
+console.log("Ejemplo 6: Obtén la suma de todos los exercises_completed, usa REDUCE")
+const result_of_exercises = explorers.reduce((acc, element) => acc + element.exercises_completed, 0)
+console.log('La suma de total de las exercises_completed: ' +result_of_exercises)
+
+console.log("Ejemplo 7: Obtén la validación si al menos uno de los explorers tiene la propiedad exercisesFinished en frontend como true, usa SOME")
+const areSomeTrue = explorers.some((b) =>  b.missions.frontend.exercisesFinished === true)
+console.log(areSomeTrue)
